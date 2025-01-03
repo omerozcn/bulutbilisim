9d359c3a0f6b703326a7c29f3e4c53f5b8f38ce3
omerozcan38 <omer.ozcan@usr.aku.edu.tr>
2025-01-03 13:33:40 +0300

    update last

--- DockerDers/app/app.py
+++ DockerDers/app/app.py
@@ -24,7 +24,6 @@ def admindestek():
     connection = create_connection()
     cursor = connection.cursor()
 
-    # Tüm destek biletlerini al
     cursor.execute("""
         SELECT id, description, photo, user_id FROM Biletler
     """)
@@ -32,12 +31,11 @@ def admindestek():
     
     connection.close()
 
-    # Fotoğraf verilerini base64 formatına çevir
     tickets_with_photo_data = []
     for ticket in tickets:
         ticket_id, description, photo, user_id = ticket
         if photo:
-            photo_data = base64.b64encode(photo).decode('utf-8')  # Base64 kodlama
+            photo_data = base64.b64encode(photo).decode('utf-8')
         else:
             photo_data = None
         tickets_with_photo_data.append((ticket_id, description, photo_data, user_id))
@@ -55,7 +53,6 @@ def girisyap():
         connection = create_connection()
         cursor = connection.cursor()
 
-        # Kullanıcı bilgilerini kontrol et ve user_id al
         cursor.execute("""
             SELECT id FROM Users
             WHERE username = ? AND password = ?
@@ -64,27 +61,27 @@ def girisyap():
 
         connection.close()
 
-        if result:  # Kullanıcı bulunduysa
+        if result:
             user_id = result[0]
             session["user_id"] = user_id
             session.permanent = True
 
-            if user_id == 1:  # Eğer user_id 1 ise admin sayfasına yönlendir
+            if user_id == 1:
                 return redirect("/admindestek")
             else:
                 return redirect("/dashboard")
         else:
-            # Hatalı kullanıcı adı veya şifre
+
             flash("Kullanıcı adı veya şifre yanlış.", "error")
             return redirect("/")
 
-    else:  # GET isteği
+    else:
         return render_template("giris.html", title="Giriş Yap")
 
 
 @app.route("/dashboard", methods=["GET"])
 def dashboard():
-    # Kullanıcı ID'sini alıyoruz
+
     user_id = session.get("user_id")
     
     if user_id:
@@ -100,7 +97,6 @@ def dashboard():
         
         if ticket:
             description, photo = ticket
-            # Fotoğraf varsa, base64 formatında döndürülmesini sağlıyoruz
             if photo:
                 photo_data = base64.b64encode(photo).decode('utf-8')
                 return render_template("dashboard.html", description=description, photo_data=photo_data)
@@ -140,10 +136,10 @@ def destek():
             connection.close()
 
             flash("Destek bileti başarıyla gönderildi.", "success")
-            return redirect("/")  # Başarıyla gönderildikten sonra yönlendirme yapılmalı
+            return redirect("/")
         else:
             flash("Kullanıcı oturumu açık değil.", "error")
-            return redirect("/girisyap")  # Oturum açılmamışsa giriş sayfasına yönlendirme
+            return redirect("/girisyap")
 
     return render_template("destek.html", title="Destek Gönder")
 
@@ -164,7 +160,7 @@ def kayitol():
                     """, (name,surname,username,password))
         
         connection.commit()
-        return redirect("/") # import
+        return redirect("/")
     else:
         return render_template("kayitol.html",title="Kayıt ol")
 
